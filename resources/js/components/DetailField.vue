edd4ff44b9e1aff1ae074888f3c45117b63ff7fd
Eperjesi Gabor <gabor.eperjesi@gmail.com>
2020-12-01 11:47:07 +0100

    Remove function from value display

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -7,7 +7,7 @@
                     <ul class="list-reset mb-4">
                         <li class="flex items-top mb-1" v-for="subRow in row">
                             <span class="block w-1/6 mr-2">{{ subRow.label }}:</span>
-                            <div class="block flex-1 prose" v-html="subRow.value.trim()"></div>
+                            <div class="block flex-1 prose" v-html="subRow.value"></div>
                         </li>
                     </ul>
                 </li>
