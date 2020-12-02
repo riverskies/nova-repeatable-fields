444ffcfec51b142fc084ac7e8fdf6ed0f23e7d5d
Barnabas Kecskes <barnabas@riverskies.com>
2020-12-02 18:34:08 +0000

    Fix repeatable field dimensions

--- resources/js/components/sub-fields/TinymceSubField.vue
+++ resources/js/components/sub-fields/TinymceSubField.vue
@@ -1,33 +1,39 @@
 <template>
-  <editor
-      v-model="localValue"
-      :api-key="subField.options.apiKey"
-      cloud-channel="5"
-      :init="initOptions"
-      :plugins="subField.options.plugins"
-      :toolbar="subField.options.toolbar"
-      :placeholder="subField.placeholder"
-      :name="subField.name"
-      @input="changed"
-  />
+    <editor
+        v-model="localValue"
+        :api-key="subField.options.apiKey"
+        cloud-channel="5"
+        :init="initOptions"
+        :plugins="subField.options.plugins"
+        :toolbar="subField.options.toolbar"
+        :placeholder="subField.placeholder"
+        :name="subField.name"
+        @input="changed"
+    />
 </template>
 
 <script>
-import Editor from '@tinymce/tinymce-vue';
+    import Editor from '@tinymce/tinymce-vue';
 
-export default {
-  components: {'editor': Editor},
-  props: ['subField', 'value'],
-  data() {
-    return {
-      initOptions: {...this.subField.options.init, input : this.changed},
-      localValue: this.value,
+    export default {
+        components: {'editor': Editor},
+        props: ['subField', 'value'],
+        data() {
+            return {
+                initOptions: {...this.subField.options.init, input: this.changed},
+                localValue: this.value,
+            };
+        },
+        methods: {
+            changed(event) {
+                this.$emit('input', this.localValue);
+            },
+        },
     };
-  },
-  methods:{
-     changed(event){
-       this.$emit('input', this.localValue);
-     }
-  }
-}
 </script>
+
+<style>
+    .tox.tox-tinymce {
+        margin-top: .5rem;
+    }
+</style>
